f936e0ba2187f8f2109b3cb51dced141180ba08d
maxsauers <msauers2@illinois.edu>
2023-07-04 23:24:56 -0500

    guessRarity

--- Server/Server.py
+++ Server/Server.py
@@ -13,19 +13,37 @@ class Server:
         }
 
         result = self.mongo_db_guess.insert_one(document)
+
+        # get percentage if correct guess
+        rarity = None
+        if correct:
+            rarity = guessRarity(player, team1, team2)
+
         if result.acknowledged:
-            return True, f"Guess saved: {player} - {team1} - {team2}"
+            return True, {"rarity": rarity}
         else:
             return False, "Database not acknowledged"
 
-    # get the rarity of a guess
-    # def guessRarity(self, player, team1, team2):
-    #     # find number of times player has guessed
-    #     query = {'$and': [
-    #         {'player': player},
-    #         {'team1': team1},
-    #         {'team2': team2}
-    #     ]}
-    #     playerGuesses = self.mongo_db_guess.find(query)
+    get the rarity of a guess
+    def guessRarity(self, player, team1, team2):
+        # find number of times player has guessed
+        queryPlayer = {'$and': [
+            {'player': player},
+            {'team1': team1},
+            {'team2': team2}
+        ]}
+        playerGuesses = self.mongo_db_guess.find(queryPlayer)
+        print(playerGuesses)
 
-        # find total number of guesses for 2 team combos
+        # find total number of guesses for 2 team combo
+        queryTeams = {'$and': [
+            {'team1': team1},
+            {'team2': team2},
+            {"correct"}: True
+        ]}
+        teamGuesses = self.mongo_db_guess.find(queryTeams)
+        print(teamGuesses)
+
+        # calculate rarity
+        rarity = playerGuesses // teamGuesses
+        return rarity
